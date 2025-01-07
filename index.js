ebe1575740ea3e6f96e3b3b8f8a0bfc550be048d
incremania <106455212+incremania@users.noreply.github.com>
2025-01-07 17:51:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
